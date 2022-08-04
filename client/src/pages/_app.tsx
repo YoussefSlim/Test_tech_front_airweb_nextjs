823b7a8a3ca8e3266eb5ed734802a4c5716a9d68
YoussefSlim <youssefslim9511@gmail.com>
2022-08-04 13:37:19 +0200

    :pencil: nextconfig: add proxy to back | swr config | create http client | config ssr

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import classNames from 'classnames';
+import { SWRConfig } from 'swr';
 
 import { Footer, Header } from '@/components';
-
 import '@/styles/globals.css';
+import { useHttpClient } from '@/hooks';
+
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const httpClient = useHttpClient();
+
   return (
-    <>
+    <SWRConfig value={{ fetcher: httpClient.get, errorRetryCount: 0 }}>
       <Header />
       <main className="min-h-mobile px-2 xl:min-h-desktop xl:w-3/5 xl:mx-auto xl:px-0 ">
         <div className={classNames('overflow-y-auto')}>
@@ -15,7 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </div>
       </main>
       <Footer />
-    </>
+    </SWRConfig>
   );
 }
 

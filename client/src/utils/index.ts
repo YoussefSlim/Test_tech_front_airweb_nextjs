670d6af2ae9732519f2f2ed91d50defb052ff6d4
YoussefSlim <youssefslim9511@gmail.com>
2022-08-04 14:29:44 +0200

    :pencil: sort categories by index | sort product by label

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -6,3 +6,10 @@ export const lowercase = (str: string) => str.replace(' ', '-').toLowerCase();
 
 // convert price from products
 export const convertPrice = (price: number) => price / 100;
+
+// sort Categories by index
+export const sortIndex = (a: { index: number }, b: { index: number }) => b.index - a.index;
+
+// sort Product by label
+export const sortLabel = (a: { label: string }, b: { label: string }) =>
+  `${a.label}`.replace(/([^\D])\s\x\s/g, '').localeCompare(b.label.replace(/([^\D])\s\x\s/g, ''));
